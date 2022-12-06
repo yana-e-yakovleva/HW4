48faa6b1c1b1839e979e1f97b8e86cd6ca92ac45
Yana Yakovleva <yana.e.yakovleva@yandex.ru>
2022-12-07 00:25:17 +0300

    task2

---
+++ Task2/Program.cs
@@ -0,0 +1,22 @@
+﻿//Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+
+bool isParsed = int.TryParse(Console.ReadLine(), out int a);
+if (!isParsed)
+{Console.WriteLine("Incorrect number");
+return;}
+
+int TotalSum (int a) 
+{
+    int sum = 0; 
+
+    while (a > 0) 
+    {
+        sum = sum + a % 10; 
+        a = a / 10; 
+    } 
+    return sum; 
+}
+
+int sum = TotalSum(a);
+
+Console.WriteLine(sum);
