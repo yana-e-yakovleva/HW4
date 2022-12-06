4b251d15f27b008725a4d2cc77f0681e43a7f42e
Yana Yakovleva <yana.e.yakovleva@yandex.ru>
2022-12-07 00:01:58 +0300

    first commit

---
+++ Task1/Program.cs
@@ -0,0 +1,36 @@
+﻿ // Напишите программу, которая принимает на вход два числа (A и B) и метод который возводит 
+ //число A в натуральную степень B. Задание должно быть выполнено в методе. В методе не должно 
+ //быть вывода в консоль. Не использовать Math.Pow()
+
+
+bool isParsed = int.TryParse(Console.ReadLine(), out int n);
+
+if (!isParsed)
+{
+    Console.WriteLine("Incorrect");
+    return -1;
+}
+
+bool isParsed1 = int.TryParse(Console.ReadLine(), out int a);
+
+if (!isParsed1)
+{
+    Console.WriteLine("Incorrect");
+    return -1;
+}
+
+int number = numberDegree(n, a);
+Console.WriteLine(number);
+
+
+int numberDegree(int n, int a)  // ЧТо он от меня хочет??
+{
+    int number = 0;
+    for (int i = 1; n <= a; i++;)
+    {
+        number = n * n;
+        return number;
+    }
+}
+
+
